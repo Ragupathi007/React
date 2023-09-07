ff446591a26a7caa92c167d4651f1d39b7fcc972
gopalakrishnan <semsampathkumar@gmail.com>
2023-09-07 19:59:07 +0530

    initial commit

---
+++ src/Animal.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import horse from "./svg/horse.svg";
+import heart from "./svg/heart.svg";
+
+import "./Animal.css";
+function AnimalShow({ type }) {
+  const svgMap = { bird, cat, cow, dog, gator, horse };
+  const [click, setClick] = useState(0);
+
+  const handleClick = () => {
+    setClick(click + 1);
+  };
+  return (
+    <div className="animal-show" onClick={handleClick}>
+      <img className="animal" alt="animals" src={svgMap[type]} />
+      <img
+        className="heart"
+        alt="heart"
+        src={heart}
+        style={{ width: 10 + 1 * click + "px" }}
+      />
+    </div>
+  );
+}
+export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./Animal";
 function App() {
+  function randomAnimal() {
+    const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+  }
+
+  //.............................................................
+  const [animals, setAnimals] = useState([]);
+  const handleClick = () => {
+    setAnimals([...animals, randomAnimal()]);
+  };
+  const renderedAnimal = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
+  //.............................................................
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        Kinder Garden
+        <button className="application ">Apply</button>
+        <p>Welocme to the World of KIDS</p>
       </header>
+
+      <button onClick={handleClick}>Add Animals</button>
+      <br />
+      <div className="animal-list">{renderedAnimal}</div>
     </div>
   );
 }
